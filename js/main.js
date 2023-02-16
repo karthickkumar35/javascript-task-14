1f6846513e6f4385c1af05e1d4888f94d67be6b4
karthickumar35 <karthickcs444@gmail.com>
2023-02-16 09:30:18 +0530

    update

---
+++ js/main.js
@@ -0,0 +1,231 @@
+let foodlist=[
+	{
+		name:"New Milan Hotel",
+		place:"North Indian, South Indian",
+		rate:"4.0",
+		time:"28 min",
+		price:"₹ 350 for two",
+		offer:"50% off | Use WELCOME50",
+		imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/aidfb8jyml1bq51y9rk0",
+	},
+	{
+		name:"Hotel RR BISMI",
+		place:"Indian, Chinese",
+		rate:"3.8",
+		time:"34 min",
+		price:"₹ 280 for two",
+		offer:"50% off | Use WELCOME50",
+		imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ii6enfkxe6uwbvw3qiar",
+	},
+	{
+		name:"Classic kitchen",
+		place:"Biryani, Indian",
+		rate:"3.7",
+		time:"37 min",
+		price:"₹ 260 for two",
+		offer:"50% off | Use WELCOME50",
+		imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/wa7qgwb6fmezpkwsubju",
+	},
+	{
+		name:"DESSERT FOREST",
+		place:"Bakery, Beverages",
+		rate:"--",
+		time:"52 min",
+		price:"₹ 300 for two",
+		offer:"50% off | Use WELCOME50",
+		imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/mfyf5xcbnokdvejpkaub",
+	},
+	{
+		name:"Yaa Rahman Biryani",
+		place:"Biryani, Indian",
+		rate:"3.5",
+		time:"52 min",
+		price:"₹ 350 for two",
+		offer:"50% off | Use WELCOME50",
+		imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/vsont2fdjveibxuqtdff",
+	},
+	{
+		name:"Shaadi Biriyani",
+		place:"Biryani, Chinese",
+		rate:"3.8",
+		time:"42 min",
+		price:"₹ 300 for two",
+		offer:"50% off | Use WELCOME50",
+		imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/opcjinwvhdde2yts7iez",
+	},
+	{
+		name:"New Al Madina",
+		place:"Biryani, Grill",
+		rate:"4.1",
+		time:"38 min",
+		price:"₹ 300 for two",
+		offer:"50% off | Use WELCOME50",
+		imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/hrzzefyekcewtpemua7j",
+	},
+	{
+		name:"Pizza 99",
+		place:"Pizzas",
+		rate:"2.6",
+		time:"50 min",
+		price:"₹ 300 for two",
+		offer:"50% off | Use WELCOME50",
+		imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/lftjuqlqzs58e9iqwj03",
+	},
+	{
+		name:"Hotel Rayyan",
+		place:"North Indian, Arabian",
+		rate:"4.2",
+		time:"36 min",
+		price:"₹ 250 for two",
+		offer:"50% off | Use WELCOME50",
+		imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/rcwgyuqxted668ehy1jo",
+	},
+	{
+		name:"ALTHAF FOOD COUR",
+		place:"Chinese, Indian",
+		rate:"3.9",
+		time:"38 min",
+		price:"₹ 200 for two",
+		offer:"50% off | Use WELCOME50",
+		imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ihqyxnrn89jbb4pkuvah",
+	},
+	{
+		name:"Salem RR Biriyani Unavagam",
+		place:"Biryani",
+		rate:"3.7",
+		time:"44 min",
+		price:"₹ 300 for two",
+		offer:"50% off | Use WELCOME50",
+		imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/eigifgugxsm1doctoesa",
+	},
+	{
+		name:"Naattarasan",
+		place:"Chettinad, North Indian",
+		rate:"4.0",
+		time:"44 min",
+		price:"₹ 400 for two",
+		offer:"50% off | Use WELCOME50",
+		imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ssugnnlqt7yzoyygtvvy",
+	},
+	{
+		name:"Milan Restaurant",
+		place:"Indian, Chinese",
+		rate:"3.9",
+		time:"33 min",
+		price:"₹ 390 for two",
+		offer:"50% off | Use WELCOME50",
+		imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/i2dorfzbdziyg6vy3ki9",
+	},
+	{
+		name:"Yaa Mohaideen Biryani",
+		place:"Biryani, Indian",
+		rate:"3.7",
+		time:"45 min",
+		price:"₹ 600 for two",
+		offer:"50% off | Use WELCOME50",
+		imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/qmf1f49v268jnxfhrpe2",
+	},
+	{
+		name:"BYNONA RESTAURANT",
+		place:"Chinese, North Indian",
+		rate:"3.2",
+		time:"33 min",
+		price:"₹ 200 for two",
+		offer:"50% off | Use WELCOME50",
+		imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/zg8ppu7s7hocmfezhwir",
+	},
+	{
+		name:"Ovenfresh Pizzas, Cakes & Desserts",
+		place:"Bakery, Pizzas",
+		rate:"3.8",
+		time:"42 min",
+		price:"₹ 300 for two",
+		offer:"50% off | Use WELCOME50",
+		imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/50a780c44b9c89b50888d6d23831f30e",
+	},
+
+];
+
+let cont = document.querySelector(".container");
+let cards = document.createElement("div");
+cards.classList.add("row");
+cont.appendChild(cards);
+
+foodlist.forEach(function(e){
+	let div = document.createElement("div");
+	div.classList.add("col");
+	cards.appendChild(div);
+	
+	let div2 = document.createElement("div");
+	div2.classList.add("col-card");
+	div.appendChild(div2);
+	
+	let img = document.createElement("img");
+	img.src=e.imgurl;
+	div2.appendChild(img);
+	
+	let name = document.createElement("h2");
+	name.innerHTML=e.name;
+	name.style.fontSize="16px";
+	name.style.margin="10px 0px"
+	div2.appendChild(name);
+	
+	let place = document.createElement("p");
+	place.innerHTML=e.place;
+	place.style.fontSize="14px";
+	place.style.margin="5px 0px";
+	place.style.color="#686b78";
+	div2.appendChild(place);
+	
+	let div3 = document.createElement("div");
+	div3.classList.add("row");
+	div3.style.color="#535665";
+	div3.style.fontWeight="500";
+	div2.appendChild(div3);
+	
+	let rate = document.createElement("p");
+	rate.innerHTML=`<iconify-icon class="icon" icon="ic:baseline-star"></iconify-icon>${e.rate}`;
+	rate.style.color="#fff";
+	rate.style.padding="2px 10px";
+	rate.style.backgroundColor="#48c479";
+	div3.appendChild(rate);
+	
+	let dot = document.createElement("p");
+	dot.innerHTML=`.`;
+	div3.appendChild(dot);
+	
+	let time = document.createElement("p");
+	time.innerHTML=e.time;
+	div3.appendChild(time);
+	
+	let dot1 = document.createElement("p");
+	dot1.innerHTML=`.`;
+	div3.appendChild(dot1);
+	
+	
+	let price = document.createElement("p");
+	price.innerHTML=e.price;
+	div3.appendChild(price);
+	
+	let offer = document.createElement("p");
+	offer.innerHTML=`<iconify-icon class="off" icon="bxs:offer"></iconify-icon>${e.offer}`;
+	offer.style.borderTop="1px solid #e9e9eb";
+	offer.style.color="#8a584b";
+	offer.style.padding="10px 0";
+	offer.style.fontSize="15px";
+	offer.style.fontWeight="600";
+	div2.appendChild(offer);
+	
+	let quick = document.createElement("p");
+	quick.innerHTML=`<a>Quick view</a>`;
+	quick.classList.add("quick");
+	quick.style.borderTop="1px solid #e9e9eb";
+	quick.style.padding="10px 0px 0px 0px";
+	quick.style.margin="10px 0px 0px 0px";
+	quick.style.textAlign="center";
+	quick.style.color="#5d8ed5";
+	quick.style.fontSize="18px";
+	quick.style.width="100%";
+	div2.appendChild(quick);
+});
+console.log(cont);
